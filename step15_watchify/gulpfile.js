8d9ced4234774f0c1aa3eaed9e34bf7c6ca1795d
윤하윤 <yhy8283@naver.com>
2023-05-08 16:06:53 +0900

    step15_watchify

--- step15_watchify/gulpfile.js
+++ step15_watchify/gulpfile.js
@@ -10,75 +10,80 @@ const sourcemaps = require('gulp-sourcemaps');
 const mergeStream = require('merge-stream');
 const gutil = require('gulp-util');
 
-//dist 폴더를 기준으로 웹서버 실행
-gulp.task('server', gulp.series('build:javascript', 'build:html', function () {
-	return browserSync.init({
-		server: {
-			baseDir: './dist'
-		}
-	});
-}));
 
 //HTML 파일을 dist 로 복사
 gulp.task('build:html', function () {
-	return gulp.src('./src/**/*.html')
-		.pipe(gulp.dest('dist'))
-		.pipe(browserSync.reload({stream: true}));
+  return gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 //자바스크립트 파일을 browserify로 번들링
 gulp.task('build:javascript', function () {
 
-	var entries = [{
-		src: 'src/js/first.js',
-		filename: 'first.js'
-	}, {
-		src: 'src/js/second.js',
-		filename: 'second.js'
-	}, {
-		src: 'src/js/third.js',
-		filename: 'third.js'
-	}];
+  const entries = [{
+    src: 'src/js/first.js',
+    filename: 'first.js'
+  }, {
+    src: 'src/js/second.js',
+    filename: 'second.js'
+  }, {
+    src: 'src/js/third.js',
+    filename: 'third.js'
+  }];
 
-	return mergeStream.apply(null, entries.map(function (entry) {
-		var target = watchify(browserify({entries: entry.src, debug: true, cache: {}, packageCache: {}}))
-			.transform(babelify, {presets: ['@babel/preset-env', '@babel/preset-react']});
+  return mergeStream.apply(null, entries.map(function (entry) {
+    const target = watchify(browserify({ entries: entry.src, debug: true, cache: {}, packageCache: {} }))
+      //.transform(babelify, { presets: ['es2015', 'react'] });
+	  .transform(babelify, {
+		presets: ['@babel/preset-env']
+	  });
 
-		var bundle = function () {
-			console.log(gutil.colors.green(entry.filename), 'bundling..');
-			console.time(gutil.colors.green(entry.filename));
+    const bundle = function () {
+      console.log(gutil.colors.green(entry.filename), 'bundling..');
+      console.time(gutil.colors.green(entry.filename));
 
-			return target.bundle()
-			.on('error', function (err) {
-				//browserify bundling 과정에서 오류가 날 경우 gulp가 죽지않도록 예외처리
-				console.error(err);
-				this.emit('end');
-			})
-			.pipe(source(entry.filename)) //vinyl object 로 변환
-			.pipe(buffer()) //buffered vinyl object 로 변환
-			.pipe(sourcemaps.init({loadMaps: true, debug: true})) //소스맵 생성 준비
-			.pipe(uglify()) //minify 해서
-			.pipe(sourcemaps.write('./')) //생성된 소스맵을 스트림에 추가
-			.pipe(gulp.dest('dist/js')) //dist 폴더에 저장
-			.on('end', function () {
-				console.timeEnd(gutil.colors.green(entry.filename));
-			})
-			.pipe(browserSync.reload({stream: true})); //browserSync 로 브라우저에 반영
-		};
+      return target.bundle()
+        .on('error', function (err) {
+          //browserify bundling 과정에서 오류가 날 경우 gulp가 죽지않도록 예외처리
+          console.error(err);
+          this.emit('end');
+        })
+        .pipe(source(entry.filename)) //vinyl object 로 변환
+        .pipe(buffer()) //buffered vinyl object 로 변환
+        .pipe(sourcemaps.init({ loadMaps: true, debug: true })) //소스맵 생성 준비
+        .pipe(uglify()) //minify 해서
+        .pipe(sourcemaps.write('./')) //생성된 소스맵을 스트림에 추가
+        .pipe(gulp.dest('dist/js')) //dist 폴더에 저장
+        .on('end', function () {
+          console.timeEnd(gutil.colors.green(entry.filename));
+        })
+        .pipe(browserSync.reload({ stream: true })); //browserSync 로 브라우저에 반영
+    };
 
-		target.on('update', bundle);
-		return bundle();
-	}));
+    target.on('update', bundle);
+    return bundle();
+  }));
 });
 
+//dist 폴더를 기준으로 웹서버 실행
+gulp.task('server', gulp.series('build:html', 'build:javascript', function () {
+	return browserSync.init({
+	  server: {
+		baseDir: './dist'
+	  }
+	});
+  }));
 
 //파일 변경 감지
 gulp.task('watch', function () {
-	gulp.watch('src/**/*.html', gulp.series('build:html'));
+  gulp.watch('src/**/*.html', gulp.series('build:html'));
+  gulp.watch('src/**/*.js', gulp.series('build:javascript'));
 });
 
 //gulp를 실행하면 default 로 minifycss task를 실행
 gulp.task('default', gulp.series('server', 'watch'));
+
 
 //- npm install gulp-uglify browser-sync browserify babelify watchify vinyl-source-stream vinyl-buffer gulp-sourcemaps merge-stream gulp-util
 
